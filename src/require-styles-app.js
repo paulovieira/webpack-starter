2202584f455ed0b0160ced056d6547d4b7cb55ac
Paulo Vieira <paulovieira@gmail.com>
2018-02-18 22:08:11 +0000

    update

---
+++ src/require-styles-app.js
@@ -0,0 +1,2 @@
+require('./test-1.css');
+require('./test-2.css');
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 let Path = require('path');
 let Webpack = require('webpack');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
 let BellOnBundlerErrorPlugin = require('bell-on-bundler-error-plugin');
 
 let internals = {};
@@ -13,10 +14,12 @@ let config = {
 
     entry: {
         'app': [
-            Path.join(internals.srcDir, "index"),
+            Path.join(internals.srcDir, 'index'),
+            Path.join(internals.srcDir, 'require-styles-app.js')
         ],
         'lib': [
             'underscore',
+            Path.join(internals.srcDir, 'require-styles-lib.js')
         ]
     },
 
@@ -25,12 +28,25 @@ let config = {
         filename: '[name].js',
     },
  
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: 'css-loader'
+                })
+            },
+        ]
+    },
+
     plugins: [
         new Webpack.optimize.CommonsChunkPlugin({
-            name: ['lib'],  // should be same as the key in the "entry" section
+            name: ['lib'],  // should be same as the key in the 'entry' section
             minChunks: Infinity
         }),
         new BellOnBundlerErrorPlugin,
+        new ExtractTextPlugin('[name].css' /*, { allChunks: true }*/)
     ],
 
 };
